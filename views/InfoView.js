ac2f9900d32d1fe621984f2f4e5906b8cdeb9b90
Fernando Rios <feriosch98@gmail.com>
2021-04-12 16:19:36 -0500

    Svg Uri Package

--- views/InfoView.js
+++ views/InfoView.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Button, SafeAreaView, ScrollView } from 'react-native';
+import SvgUri from 'expo-svg-uri';
 import { default as kanjiJson } from '../assets/data/kanji_info.json';
 import { DEVICE_HEIGHT, DEVICE_WIDTH, STATUS_BAR_HEIGHT } from '../dimensions'
 
 const InfoView = ({ kanji, setSelectedKanji }) => {
 
-  const [kanjiInfo, setKanjiInfo] = useState(`Loading ${kanji}...`);
+  const [kanjiInfo, setKanjiInfo] = useState(null);
 
   const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -17,31 +18,44 @@ const InfoView = ({ kanji, setSelectedKanji }) => {
   useEffect(() => {
     delay(2000)
       .then(() => {
-        setKanjiInfo(JSON.stringify(kanjiJson));
+        setKanjiInfo(kanjiJson);
       })
   }, [])
 
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Go Back"
-        onPress={() => onClickBack()}
-        style={styles.backButton}
-      />
-      <Text style={styles.mainKanji}>
-        {kanji}
-      </Text>
-      <SafeAreaView style={styles.scrollContainer}>
-        <ScrollView>
-          <Text>
-            { kanjiInfo.toString() }
-          </Text>
-        </ScrollView>
-      </SafeAreaView>
-      <Text>
-      </Text>
-    </View>
-  );
+  if (kanjiInfo === null) {
+    return (
+      <View>
+        <Text>
+          { `Loading ${kanji}...` }
+        </Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Go Back"
+          onPress={() => onClickBack()}
+          style={styles.backButton}
+        />
+        <Text style={styles.mainKanji}>
+          {kanji}
+        </Text>
+        <SafeAreaView style={styles.scrollContainer}>
+          <ScrollView>
+            <Text>
+              { JSON.stringify(kanjiInfo) }
+            </Text>
+            <SvgUri
+              source={{ uri: kanjiInfo.kanji.strokes.images[0] }}
+            />
+          </ScrollView>
+        </SafeAreaView>
+        <Text>
+        </Text>
+      </View>
+    );
+  }
 
 };
 
